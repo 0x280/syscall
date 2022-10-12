e6ee07f2840d50dcf8b4fea0e27b59bb105d2c8e
x280 <0x280@protonmail.com>
2022-10-12 13:27:53 +0200

    added proper caching of all syscalls at first call

--- include/syscall.hpp
+++ include/syscall.hpp
@@ -82,18 +82,20 @@ namespace syscall
     inline std::mutex mtx;
     inline std::vector<std::pair<std::uint64_t, std::uint32_t>> syscallCache{};
 
-    template <std::uint64_t syscall_hash, class syscall_type>
-    SYSCALL_FORCEINLINE syscall_type get()
+    SYSCALL_FORCEINLINE void setup_cache()
     {
+        std::lock_guard<std::mutex> lock(mtx);
+
+        if (!syscallCache.empty())
+            syscallCache.clear();
+
         // const/static vars
         const std::uint64_t ntdll_hash = strhash(L"ntdll.dll");
 
         // vars
-        PPEB peb = ((PPEB)__readgsqword(0x60));
+        PPEB peb = reinterpret_cast<PPEB>(__readgsqword(0x60));
         PPEB_LDR_DATA ldr = peb->Ldr;
-
         PVOID ntdllBase = nullptr;
-
         PIMAGE_DOS_HEADER dosHeaders = nullptr;
         PIMAGE_NT_HEADERS ntHeaders = nullptr;
         PIMAGE_EXPORT_DIRECTORY exportDir = nullptr;
@@ -101,26 +103,11 @@ namespace syscall
         PDWORD namesPtr = nullptr;
         PWORD namesOrdinalPtr = nullptr;
 
-        PVOID exportAddr = nullptr;
-        std::uint32_t sc = 0;
-
-        std::lock_guard lg(mtx);
-
-        for (const auto &entry : syscallCache)
-        {
-            if (entry.first == syscall_hash)
-            {
-                sc = entry.second;
-                goto cleanup;
-            }
-        }
-
-        // get ntdll base
+        // find ntdll.dll
         for (PLIST_ENTRY pListEntry = ldr->InMemoryOrderModuleList.Flink; pListEntry != &ldr->InMemoryOrderModuleList; pListEntry = pListEntry->Flink)
         {
             if (!pListEntry)
                 continue;
-
             PLDR_DATA_TABLE_ENTRY pEntry = CONTAINING_RECORD(pListEntry, LDR_DATA_TABLE_ENTRY, InMemoryOrderLinks);
             if (strhash(pEntry->BaseDllName.Buffer, pEntry->BaseDllName.Length / sizeof(wchar_t)) == ntdll_hash)
             {
@@ -129,59 +116,71 @@ namespace syscall
             }
         }
         if (ntdllBase == nullptr)
-            goto cleanup;
+            return;
 
         // get function export
         dosHeaders = (PIMAGE_DOS_HEADER)ntdllBase;
         if (dosHeaders->e_magic != IMAGE_DOS_SIGNATURE)
-            goto cleanup;
+            return;
 
         ntHeaders = (PIMAGE_NT_HEADERS)((PBYTE)dosHeaders + dosHeaders->e_lfanew);
         if (ntHeaders->Signature != IMAGE_NT_SIGNATURE)
-            goto cleanup;
+            return;
 
         exportDir = (PIMAGE_EXPORT_DIRECTORY)((PBYTE)dosHeaders + ntHeaders->OptionalHeader.DataDirectory[0].VirtualAddress);
         functionsPtr = (PDWORD)((PBYTE)dosHeaders + exportDir->AddressOfFunctions);
         namesPtr = (PDWORD)((PBYTE)dosHeaders + exportDir->AddressOfNames);
         namesOrdinalPtr = (PWORD)((PBYTE)dosHeaders + exportDir->AddressOfNameOrdinals);
 
+        // enumerate exports
         for (std::uint16_t i = 0; i < exportDir->NumberOfNames; i++)
         {
             auto funcName = (PCHAR)((PBYTE)dosHeaders + namesPtr[i]);
             auto funcAddr = (PVOID)((PBYTE)dosHeaders + functionsPtr[namesOrdinalPtr[i]]);
             auto funcHash = strhash(funcName);
 
-            if (funcHash == syscall_hash)
+            // extract syscall, pasted from https://github.com/am0nsec/HellsGate
+            for (std::uint16_t i = 0; true; i++)
             {
-                exportAddr = funcAddr;
-                break;
+                if (*((PBYTE)exportAddr + i) == 0x0F && *((PBYTE)exportAddr + i + 1) == 0x05)
+                    break;
+
+                if (*((PBYTE)exportAddr + i) == 0xC3)
+                    break;
+
+                if (*((PBYTE)exportAddr + i) == 0x4C && *((PBYTE)exportAddr + i + 1) == 0x8B && *((PBYTE)exportAddr + i + 2) == 0xD1 &&
+                    *((PBYTE)exportAddr + i + 3) == 0xB8 && *((PBYTE)exportAddr + i + 6) == 0x00 && *((PBYTE)exportAddr + i + 7) == 0x00)
+                {
+                    BYTE high = *((PBYTE)exportAddr + 5 + i);
+                    BYTE low = *((PBYTE)exportAddr + 4 + i);
+                    std::uint32_t syscallNr = (high << 8) | low;
+
+                    if (syscallNr != 0) {
+                        // insert syscall into cache
+                        syscallCache.push_back(std::make_pair(funcHash, syscallNr));
+                        continue;
+                    }
+                }
             }
         }
-        if (exportAddr == nullptr)
-            goto cleanup;
+    }
 
-        // extract syscall, pasted from https://github.com/am0nsec/HellsGate
-        for (std::uint16_t i = 0; true; i++)
+    template <std::uint64_t syscall_hash, class syscall_type>
+    SYSCALL_FORCEINLINE syscall_type get()
+    {
+        std::uint32_t sc = 0;
+
+        if (syscallCache.empty())
+            setup_cache();
+
+        std::lock_guard lg(mtx);
+        for (const auto &entry : syscallCache)
         {
-            if (*((PBYTE)exportAddr + i) == 0x0F && *((PBYTE)exportAddr + i + 1) == 0x05)
-                break;
-
-            if (*((PBYTE)exportAddr + i) == 0xC3)
-                break;
-
-            if (*((PBYTE)exportAddr + i) == 0x4C && *((PBYTE)exportAddr + i + 1) == 0x8B && *((PBYTE)exportAddr + i + 2) == 0xD1 &&
-                *((PBYTE)exportAddr + i + 3) == 0xB8 && *((PBYTE)exportAddr + i + 6) == 0x00 && *((PBYTE)exportAddr + i + 7) == 0x00)
+            if (entry.first == syscall_hash)
             {
-                BYTE high = *((PBYTE)exportAddr + 5 + i);
-                BYTE low = *((PBYTE)exportAddr + 4 + i);
-                sc = (high << 8) | low;
-                break;
+                sc = entry.second;
+                goto cleanup;
             }
-        }
-
-        if (sc != 0)
-        {
-            syscallCache.push_back({syscall_hash, sc});
         }
 
     cleanup:
